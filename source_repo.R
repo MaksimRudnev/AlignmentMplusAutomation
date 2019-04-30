091c56080ec96c63a7b87c2eb0c7a54d9112c9c8
Maksim Rudnev <maksim.rudnev@gmail.com>
2019-04-30 15:53:52 +0100

    source batch

---
+++ source_repo.R
@@ -0,0 +1,3 @@
+source('https://raw.githubusercontent.com/MaksimRudnev/AlignmentMplusAutomation/master/runAlignment.R')
+source('https://raw.githubusercontent.com/MaksimRudnev/AlignmentMplusAutomation/master/extractAlignment.R')
+source('https://raw.githubusercontent.com/MaksimRudnev/AlignmentMplusAutomation/master/extractAlignmentSim.R')
